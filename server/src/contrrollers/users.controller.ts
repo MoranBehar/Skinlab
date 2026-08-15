453cb2bafa74c7a32eee66d549ba745f77e2bb33
Moran Behar <moran.behar@gmail.com>
2026-08-15 19:57:53 +0300

    Require admin role for user lookup

--- server/src/contrrollers/users.controller.ts
+++ server/src/contrrollers/users.controller.ts
@@ -1,13 +1,24 @@
-import { Controller, Get, Put, Body, UseGuards,Param,ParseIntPipe,Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Put,
+  Body,
+  UseGuards,
+  Param,
+  ParseIntPipe,
+  Delete,
+} from '@nestjs/common';
 import * as bcrypt from 'bcrypt';
 import { UsersService } from '../services/users.service';
 import { UpdateUserDto } from '../DTO/users/updateUser.dto';
 import { JwtAuthGuard } from '../common/guards/jwtAuth.guard';
+import { RolesGuard } from '../common/guards/roles.guard';
 import { GetUser } from '../common/decorators/getUser.decorator';
+import { Roles } from '../common/decorators/roles.decorator';
 
 @Controller('users')
 //every endpoint demaend loging in
-@UseGuards(JwtAuthGuard) 
+@UseGuards(JwtAuthGuard)
 export class UsersController {
   constructor(private usersService: UsersService) {}
 
@@ -17,12 +28,12 @@ export class UsersController {
 
     let userData;
 
-    if(user) {
-        //not returning password and token
-        const { password, access_token, ...userProfile } = user;
-        userData = userProfile;
+    if (user) {
+      //not returning password and token
+      const { password, access_token, ...userProfile } = user;
+      userData = userProfile;
     }
-    
+
     return userData;
   }
 
@@ -37,22 +48,24 @@ export class UsersController {
     }
 
     const updatedUser = await this.usersService.update(userId, updateUserDto);
-    
+
     //not returning password and token
     const { password, access_token, ...userProfile } = updatedUser;
     return userProfile;
   }
 
   @Get(':id')
+  @UseGuards(RolesGuard)
+  @Roles(1)
   async getUserById(@Param('id', ParseIntPipe) id: number) {
     const user = await this.usersService.findById(id);
-    
+
     let userData;
 
-    if(user) {
-        //not returning password and token
-        const { password, access_token, ...userProfile } = user;
-        userData = userProfile;
+    if (user) {
+      //not returning password and token
+      const { password, access_token, ...userProfile } = user;
+      userData = userProfile;
     }
 
     return userData;
